b22cd9e60bd78719306f9b4fe2957973ace0132b
Celso Coutinho <20.celso@gmail.com>
2018-03-20 01:10:29 +0100

    Add files via upload
    First module assignment finished. The task consisted in creating a csv to json converter

---
+++ Module-1/csv2json.js
@@ -0,0 +1,42 @@
+const readLine = require('readline')
+const fs = require("fs")
+
+const csv2json = (fileName = 'customer-data') => {
+
+	var count = 0, nVars = 0;
+	var variables = []
+	var entries = []
+	
+	var lineReader = readLine.createInterface({
+	  input: fs.createReadStream(fileName + ".csv")
+	});
+
+	lineReader.on('line', (line, callback) => {
+	  
+	  if(count == 0) {  // First line
+		
+		variables = line.split(",")
+		nVars = variables.length
+	  }
+	  else {  // remaining lines
+		
+		var values = line.split(",")
+		
+		var entry = new Object()
+		for( var i=0; i<nVars; i++) {
+			entry[variables[i]] = values[i]
+		}
+
+		entries.push(entry);
+	  }
+	  
+	  count++;
+	})
+	
+	lineReader.on('close', () => {
+		
+		fs.writeFileSync(fileName + ".json", JSON.stringify(entries) , 'utf-8'); 
+	})
+}
+
+csv2json(process.argv[2])
